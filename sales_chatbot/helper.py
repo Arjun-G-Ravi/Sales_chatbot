9f3ead85905c340c4bdc70156c3460e014923126
Arjun-G-Ravi <arjungravi1@gmail.com>
2024-07-18 20:52:46 +0530

    Committed

--- sales_chatbot/helper.py
+++ sales_chatbot/helper.py
@@ -13,8 +13,8 @@ class LLM:
         '''Generates output using Google API, given the input.'''
         chat_completion = self.client.chat.completions.create(
             messages=[{"role": "user","content": f"{inp}"}],
-            model="llama3-8b-8192",
-            # All models: llama3-8b-8192 llama3-70b-8192 gemma-7b-it mixtral-8x7b-32768
+            model="llama3-70b-8192",
+            # Other models: llama3-8b-8192 llama3-70b-8192 gemma-7b-it mixtral-8x7b-32768
             temperature = self.temperature,
             top_p=self.top_p)
 
@@ -22,16 +22,14 @@ class LLM:
 
 def extract_from_spreadsheet(sheet_link):
     df = pd.read_csv(sheet_link)
-    # print(df.describe())
-    # print(df.info())
     return df
 
 def convert_to_pdf(string):
     pdf = fpdf.FPDF()
     pdf.add_page()
-    pdf.set_font("Arial", size=12)
+    pdf.set_font("Arial", size=14)
     pdf.multi_cell(0, 5, txt=string)
-    pdf.output("output.pdf", "F")
+    pdf.output("output/output.pdf", "F")
 
 
 def custom_information_extraction(df):
